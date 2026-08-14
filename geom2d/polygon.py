57064a0cef5a3a04078c2f41f5919ff49dad6815
Jose A. Rosas Cordova <jarosascordova@gmail.com>
2026-08-14 10:36:14 +0200

    Create polygon class and its tests

---
+++ geom2d/polygon.py
@@ -0,0 +1,27 @@
+import operator
+from functools import reduce
+
+from geom2d.point import Point
+from geom2d.segment import Segment
+from utils.pairs import make_round_pairs
+
+
+class Polygon:
+    def __init__(self, vertices: [Point]):
+        if len(vertices) < 3:
+            raise ValueError('Need 3 or more vertices')
+
+        self.vertices = vertices
+
+    def sides(self):
+        vertex_pairs = make_round_pairs(self.vertices)
+        return [Segment(pair[0], pair[1]) for pair in vertex_pairs]
+
+    @property
+    def centroid(self):
+        vtx_count = len(self.vertices)
+        vtx_sum = reduce(operator.add, self.vertices)
+        return Point(
+            vtx_sum.x / vtx_count,
+            vtx_sum.y / vtx_count
+        )
