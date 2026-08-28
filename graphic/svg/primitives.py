a5a56946124f105096eb79fbff7b56455a9e7b2b
Jose A. Rosas Cordova <jarosascordova@gmail.com>
2026-08-28 17:27:09 +0200

    Complete svg sub-package

--- graphic/svg/primitives.py
+++ graphic/svg/primitives.py
@@ -1,9 +1,14 @@
-from geom2d import Rect, Segment
+from geom2d import Circle, Rect, Segment, Point, Polygon, Vector
 from graphic.svg.attributes import attrs_to_str
 from graphic.svg.read import read_template
 
 __segment_template = read_template("line")
 __rect_template = read_template("rect")
+__circle_template = read_template("circle")
+__polygon_template = read_template("polygon")
+__polyline_template = read_template("polyline")
+__text_template = read_template("text")
+__group_template = read_template("group")
 
 
 def segment(seg: Segment, attributes=()):
@@ -22,3 +27,66 @@ def rectangle(rect: Rect, attributes=()):
         .replace("{{width}}", str(rect.size.width)) \
         .replace("{{height}}", str(rect.size.height)) \
         .replace("{{attrs}}", attrs_to_str(attributes))
+
+
+def circle(circ: Circle, attributes=()):
+    return __circle_template \
+        .replace("{{cx}}", str(circ.center.x)) \
+        .replace("{{cy}}", str(circ.center.y)) \
+        .replace("{{r}}", str(circ.radius)) \
+        .replace("{{attrs}}", attrs_to_str(attributes))
+
+
+def polygon(pol: Polygon, attributes=()):
+    return __polygon_template \
+        .replace("{{points}}", __format_points(pol.vertices)) \
+        .replace("{{attrs}}", attrs_to_str(attributes))
+
+
+def __format_points(points: [Point]):
+    return " ".join([f"{p.x},{p.y}" for p in points])
+
+
+def polyline(points: [Point], attributes=()):
+    return __polyline_template \
+        .replace("{{points}}", __format_points(points)) \
+        .replace("{{attrs}}", attrs_to_str(attributes))
+
+
+def text(txt: str, pos: Point, disp: Vector, attrs_list=()):
+    return __text_template \
+        .replace("{{x}}", str(pos.x)) \
+        .replace("{{y}}", str(pos.y)) \
+        .replace("{{dx}}", str(disp.u)) \
+        .replace("{{dy}}", str(disp.v)) \
+        .replace("{{text}}", txt) \
+        .replace("{{attrs}}", attrs_to_str(attrs_list))
+
+
+def group(primitives: [str], attributes=()):
+    return __group_template \
+        .replace("{{content}}", "\n".join(primitives)) \
+        .replace("{{attrs}}", attrs_to_str(attributes))
+
+
+def arrow(
+        _segment: Segment,
+        length: float,
+        height: float,
+        attributes=()
+):
+    director = _segment.direction_vector
+    v_l = director.opposite().with_length(length)
+    v_h1 = director.perpendicular().with_height(height / 2.0)
+    v_h2 = v_h1.opposite()
+
+    return group([
+        segment(_segment),
+        polyline([
+            _segment.end.displaced(v_l + v_h1),
+            _segment.end,
+            _segment.end.displaced(v_l + v_h2)
+        ])
+    ],
+        attributes
+    )
